db955c9385adb824aa7e409ce38f08c85fad5091
viktoriapashkovskaya <vika.vabishchevich.96@mail.ru>
2023-05-08 08:24:55 +0200

    Singleton added

---
+++ src/main/java/util/DriverInit.java
@@ -0,0 +1,35 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class DriverInit {
+
+    private static DriverInit instanceDriver  = null;
+    private WebDriver driver;
+
+    private DriverInit() {
+
+    }
+
+    public synchronized static DriverInit getInstance() {
+        if (instanceDriver == null) {
+            instanceDriver = new DriverInit();
+        }
+        return instanceDriver;
+    }
+
+    public WebDriver getDriver(){
+        ChromeOptions handlingSSL = new ChromeOptions();
+        handlingSSL.setAcceptInsecureCerts(true);
+        handlingSSL.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(handlingSSL);
+        driver.manage().timeouts().implicitlyWait(2, SECONDS);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
